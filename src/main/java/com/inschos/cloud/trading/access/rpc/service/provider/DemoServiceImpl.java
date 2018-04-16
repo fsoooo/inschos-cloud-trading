94c91d3f8c791fd66b21eed659329efbb33393bc
meiming <meiming@bigwei.com>
2018-04-16 19:25:49 +0800

    rpc  account

--- src/main/java/com/inschos/cloud/trading/access/rpc/service/provider/DemoServiceImpl.java
+++ src/main/java/com/inschos/cloud/trading/access/rpc/service/provider/DemoServiceImpl.java
@@ -1,12 +1,12 @@
 package com.inschos.cloud.trading.access.rpc.service.provider;
 
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.inschos.cloud.trading.access.rpc.service.DemoService;
 import com.inschos.cloud.trading.assist.kit.L;
 import com.inschos.cloud.trading.data.mapper.AreaMapper;
 import com.inschos.cloud.trading.model.Area;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by IceAnt on 2018/3/6.
