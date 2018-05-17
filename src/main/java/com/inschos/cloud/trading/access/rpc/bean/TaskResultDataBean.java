568fde842912943d95449b90c8dd4394889c5b98
zhangyunhe <zyhf4@vip.qq.com>
2018-05-17 14:07:18 +0800

    修改部分代码

---
+++ src/main/java/com/inschos/cloud/trading/access/rpc/bean/TaskResultDataBean.java
@@ -0,0 +1,16 @@
+package com.inschos.cloud.trading.access.rpc.bean;
+
+/**
+ * 创建日期：2018/5/17 on 11:39
+ * 描述：
+ * 作者：zhangyunhe
+ */
+public class TaskResultDataBean {
+
+    public String dataType;
+    public String dataAmount;
+    public String userId;
+    public String userType;
+    public String time;
+
+}
---
+++ src/main/java/com/inschos/cloud/trading/access/rpc/client/TaskResultClient.java
@@ -0,0 +1,41 @@
+package com.inschos.cloud.trading.access.rpc.client;
+
+import com.inschos.cloud.trading.access.rpc.bean.ProductBean;
+import com.inschos.cloud.trading.access.rpc.bean.TaskResultDataBean;
+import com.inschos.cloud.trading.access.rpc.service.ProductService;
+import com.inschos.cloud.trading.access.rpc.service.TaskResultService;
+import com.inschos.cloud.trading.assist.kit.L;
+import hprose.client.HproseHttpClient;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
+
+/**
+ * 创建日期：2018/5/17 on 13:51
+ * 描述：
+ * 作者：zhangyunhe
+ */
+public class TaskResultClient {
+
+    @Value("${rpc.remote.task.host}")
+    private String host;
+
+    private final String uri = "/rpc/task";
+
+
+    private TaskResultService getService() {
+        return new HproseHttpClient(host + uri).useService(TaskResultService.class);
+    }
+
+    public long updateTaskResult(TaskResultDataBean taskResultDataBean) {
+        try {
+            TaskResultService service = getService();
+            return service != null ? service.updateTaskResult(taskResultDataBean) : -1;
+
+        } catch (Exception e) {
+            L.log.error("remote fail {}", e.getMessage(), e);
+            return -1;
+        }
+    }
+
+}
