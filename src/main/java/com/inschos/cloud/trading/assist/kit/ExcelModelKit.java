cf356dbb66b7dbda83c698cd96014d5ed85660a7
zhangyunhe <zyhf4@vip.qq.com>
2018-06-05 15:44:30 +0800

    修改部分代码

--- src/main/java/com/inschos/cloud/trading/assist/kit/ExcelModelKit.java
+++ src/main/java/com/inschos/cloud/trading/assist/kit/ExcelModelKit.java
@@ -201,6 +201,7 @@ public class ExcelModelKit {
 
         for (int i = 0; i < columnNum; i++) {
             sheet.autoSizeColumn(i);
+            sheet.setColumnWidth(i, sheet.getColumnWidth(i) * 17 / 10);
         }
     }
 
